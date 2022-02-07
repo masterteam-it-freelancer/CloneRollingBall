5de42c02d823b640719f8af2029de3a9eb27df7e
MasterTeam IT Freelancer <59349374+masterteam-it-freelancer@users.noreply.github.com>
2022-02-07 15:46:46 +0700

    Create Repos

--- Source/RollingBall/RollingBall.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RollingBall.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RollingBall, "RollingBall" );
